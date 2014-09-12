e8bad97810284360378257ce8317016e5ed3a54e
David Cantrell <david@cantrell.org.uk>
2014-09-12 21:55:16 +0100

    configgerable sort order; help text; doco; licence; kitchen sink; dog; yeti scarer

--- mkpodcasts.pl
+++ mkpodcasts.pl
@@ -1,5 +1,45 @@
 #!/home/david/perl5/perlbrew/perls/perl-5.20.0/bin/perl
 
+=head1 NAME
+
+mkpodcasts
+
+=head1 DESCRIPTION
+
+Turn a directory of media files into a podcast
+
+=head1 ARGUMENTS
+
+The first three arguments are mandatory.
+
+=head2 --source
+
+The directory to get files from. Currently files ending in .mp3, .m4a, .mp4,
+or .m4v are considered media files.
+
+=head2 --target
+
+The directory to put the podcast in. This must not be the same as the source,
+but must be on the same filesystem because hard-links are created for the
+media files. A 'feed.xml' file is also created.
+
+=head2 --httpdir
+
+The address in HTTP-land of the directory containing the podcast
+
+=head2 --sortby (optional)
+
+Podcast items will be sorted by this field. Valid values are 'mtime' and 'name'.
+
+=head1 AUTHOR, LICENCE, ETC
+
+Written by David Cantrell <david@cantrell.org.uk>.
+
+You may use, modify and distribute this software in accordance with the
+terms laid out in the GNU General Public Licence version 2.
+
+=cut
+
 use strict;
 use warnings;
 
@@ -9,18 +49,29 @@ use MIME::Types;
 use File::Slurp;
 use Getopt::Long;
 use HTTP::Date;
+use Pod::Usage;
 
-my($source, $target, $httpdir);
+my $sortby = 'mtime';
+my($source, $target, $httpdir, $help);
 
 GetOptions(
-    'source=s' => \$source,
-    'target=s' => \$target,
+    'source=s'  => \$source,
+    'target=s'  => \$target,
+    'sortby=s'  => \$sortby,
     'httpdir=s' => \$httpdir,
+    'help|?'    => \$help,
 );
+pod2usage(0) if($help);
 
-die("source must be a directory\n") unless(-d $source);
-die("target must be a directory\n") unless(-d $target);
-die("httpdir must be sane\n") unless($httpdir =~ /^https?:\/\//);
+my %sorters = (
+    mtime => sub { (stat($_[0]))[9] <=> (stat($_[1]))[9] },
+    name  => sub { $_[0] cmp $_[1] },
+);
+pod2usage({ -message => "source must be a directory", -exitval => 1 }) unless(-d $source);
+pod2usage({ -message => "target must be a directory\n", -exitval => 1 }) unless(-d $target);
+pod2usage({ -message => "httpdir must be sane\n", -exitval => 1 }) unless($httpdir =~ /^https?:\/\//);
+pod2usage({ -message => "sortby must be one of [".join(', ', sort keys %sorters)."]", -exitval => 1 })
+    unless($sortby = $sorters{$sortby});
 
 opendir(SOURCE, $source) || die("Can't read $source\n");
 my @files = grep { -f "$source/$_" && $_ =~ /\.(mp3|m4a|mp4|m4v)$/ } readdir(SOURCE);
@@ -39,16 +90,17 @@ Template->new()->process(
         description => "The media files from $source",
         pubdate => time2str(),
         items => [ map {
-            my ($size, $mtime) = (stat("$target/$_"))[7, 9];
+            my ($size, $mtime) = (stat($_))[7, 9];
+            (my $url = $_) =~ s/^$target/$httpdir/;
             {
                 title => $_,
                 description => $_,
                 pubdate => time2str($mtime),
                 size => $size,
-                url => "$httpdir/$_",
-                mime => MIME::Types->new()->mimeTypeOf("$target/$_"),
+                url => $url,
+                mime => MIME::Types->new()->mimeTypeOf($_),
             }
-        } sort { (stat("$target/$a"))[9] <=> (stat("$target/$b"))[9] } @files ]
+        } sort { $sortby->($a, $b) } map { "$target/$_" } @files ]
     },
     "$target/feed.xml"
 );
