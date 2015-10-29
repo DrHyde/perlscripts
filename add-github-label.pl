6122244c9505c722dbf576fbe1894b69a7bc4e3f
David Cantrell <david@cantrell.org.uk>
2015-10-29 21:43:08 +0000

    use auth on all requests to prevent rate-limiting

--- add-github-label.pl
+++ add-github-label.pl
@@ -1,9 +1,10 @@
+#!/usr/bin/env perl
+
 use strict;
 use warnings;
 
 use JSON::Parse qw(parse_json);
 use LWP::UserAgent;
-use MIME::Base64;
 
 my($noisy, $label, $colour, $owner, $user, $pass) = (1);
 
@@ -30,7 +31,7 @@ my $ua = LWP::UserAgent->new();
 my $next_url = "https://api.github.com/users/$owner/repos";
 
 while($next_url) {
-    my $req = HTTP::Request->new(GET => $next_url);
+    my $req = HTTP::Request::WithAuth->new(GET => $next_url);
     print "  fetching $next_url\n" if($noisy);
     my $res = $ua->request($req);
     if(!$res->is_success()) {
@@ -47,26 +48,35 @@ while($next_url) {
     my @repos = grep { !$_->{fork} } @{$repos};
     
     REPO: foreach my $repo (@repos) {
-	my $full_name = $repo->{full_name};
+        my $full_name = $repo->{full_name};
         (my $labels_url = $repo->{labels_url}) =~ s/\{.*//;
-	my $check_label_exists_req = HTTP::Request->new(GET => "$labels_url/$label");
-	my $res = $ua->request($check_label_exists_req);
-	if($res->is_success()) {
-	    print "Label '$label' already exists for $full_name, skipping\n";
-	    next REPO;
-	} else {
-	    my $create_label_req = HTTP::Request->new(POST => "$labels_url");
-	    $create_label_req->header('Content-Type'  => 'application/json');
-	    $create_label_req->header('Authorization' => 'Basic '.encode_base64("$user:$pass"));
-	    # use Data::Dumper;
-	    # die(Dumper($create_label_req));
-	    $create_label_req->content('{"name":"'.$label.'","color":"'.$colour.'"}');
-	    my $res = $ua->request($create_label_req);
-	    if(!$res->is_success()) {
-	        die("Couldn't post to $labels_url: ".$res->status_line()."\n".$res->content()."\n");
-	    } else {
-	        print "Created label $label with colour $colour on $full_name\n";
-	    }
-	}
+        my $check_label_exists_req = HTTP::Request::WithAuth->new(GET => "$labels_url/$label");
+        my $res = $ua->request($check_label_exists_req);
+        if($res->is_success()) {
+            print "Label '$label' already exists for $full_name, skipping\n";
+            next REPO;
+        } else {
+            my $create_label_req = HTTP::Request::WithAuth->new(POST => "$labels_url");
+            $create_label_req->header('Content-Type'  => 'application/json');
+            # use Data::Dumper;
+            # die(Dumper($create_label_req));
+            $create_label_req->content('{"name":"'.$label.'","color":"'.$colour.'"}');
+            my $res = $ua->request($create_label_req);
+            if(!$res->is_success()) {
+                die("Couldn't post to $labels_url: ".$res->status_line()."\n".$res->content()."\n");
+            } else {
+                print "Created label $label with colour $colour on $full_name\n";
+            }
+        }
     }
+}
+
+package HTTP::Request::WithAuth;
+use MIME::Base64;
+use base 'HTTP::Request';
+sub new {
+    my($class, @args) = @_;
+    my $req = $class->SUPER::new(@args);
+    $req->header('Authorization' => 'Basic '.encode_base64("$user:$pass"));
+    return $req;
 }
